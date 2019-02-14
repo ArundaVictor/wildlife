2989d4d3948958cd00a5adf13f3875fdf03ee8b4
Arunda Victor <victoroyugi31@gmail.com>
2019-02-14 16:24:35 +0300

    add insert and delete functions

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,6 @@
+import org.sql2o.*;
+
+
 public abstract class Animal{
 	public String name;
 	public int id;
@@ -10,4 +13,35 @@ public abstract class Animal{
 
   public int getId(){
     return id;
+  }
+
+
+   @Override
+  public boolean equals(Object otherAnimal){
+    if (!(otherAnimal instanceof Animal)) {
+      return false;
+    } else {
+      Animal newAnimal = (Animal) otherAnimal;
+      return this.getName().equals(newAnimal.getName()) &&
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO animals (name, type) VALUES (:name, :type)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("type", this.type)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+    String sql = "DELETE FROM animals WHERE id = :id;";
+    con.createQuery(sql)
+      .addParameter("id", this.id)
+      .executeUpdate();
+    }
   }
