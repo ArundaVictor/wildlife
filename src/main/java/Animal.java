1cb0549f61a7dc0fcaef0ab035a830bfef58315c
Arunda Victor <victoroyugi31@gmail.com>
2019-02-14 16:42:51 +0300

    update class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -5,7 +5,7 @@ public abstract class Animal{
 	public String name;
 	public int id;
 	public String type;
-}
+
 
  public String getName(){
     return name;
@@ -45,3 +45,8 @@ public abstract class Animal{
       .executeUpdate();
     }
   }
+
+
+
+
+}
