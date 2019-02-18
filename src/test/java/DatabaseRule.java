f5df43af1c84b2430af23f1272e3140b8ec28c02
Arunda Victor <victoroyugi31@gmail.com>
2019-02-18 12:50:50 +0300

    update databse rule

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -6,7 +6,19 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "arunda", "arunda");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "arunda", "arunda");
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteAnimalsQuery = "DELETE FROM animals *;";
+      String deleteEndangeredQuery = "DELETE FROM endangered_animals *;";
+      String deleteSightingsQuery = "DELETE FROM sightings *;";
+      con.createQuery(deleteAnimalsQuery).executeUpdate();
+      con.createQuery(deleteEndangeredQuery).executeUpdate();
+       con.createQuery(deleteSightingsQuery).executeUpdate();
+    }
   }
 
 }
