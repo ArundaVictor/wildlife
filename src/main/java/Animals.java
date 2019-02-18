d81170e8569990f5176f406888c5d142daab9ebe
Arunda Victor <victoroyugi31@gmail.com>
2019-02-18 07:54:14 +0300

    add abstract class

---
+++ src/main/java/Animals.java
@@ -0,0 +1,16 @@
+public class Animal {
+  public String name;
+  public int id;
+
+  public Animal(String name) {
+    this.name = name;
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
