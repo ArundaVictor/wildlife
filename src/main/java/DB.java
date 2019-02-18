923662b944ab1f0f0e6c3aa02a370609458bf67a
Arunda Victor <victoroyugi31@gmail.com>
2019-02-18 12:45:08 +0300

    update DB.java

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,34 @@
-import org.sql2o.*;
+// import org.sql2o.*;
 
-public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "arunda", "arunda");
+// public class DB {
+//   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker_test", "arunda", "arunda");
+// }
+
+import org.sql2o.*; 
+import java.net.URI; 
+import java.net.URISyntaxException;
+import org.slf4j.Logger;                       
+import org.slf4j.LoggerFactory;
+public class DB { 
+    private static URI dbUri; 
+    public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
+    
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == "arunda") ? "arunda" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == "arunda") ? "arunda" : dbUri.getUserInfo().split(":")[1];
+sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) { 
+            logger.error("Unable to connect to database.");     
+        }
+    }
 }
